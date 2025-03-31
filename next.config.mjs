409ad56033c376dd3d1c4993f5ca134ea938c87e
darshansatapara <darshansatapara286@gmail.com>
2025-03-31 20:20:22 +0530

    Refactor next.config.mjs to remove experimental settings and simplify configuration. Update axios.js to change base URL for API requests from localhost to production endpoint. Remove '@rc-component/util' dependency from package.json and package-lock.json for cleaner dependency management.

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,4 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    esmExternals: false, // Forces Next.js to use CommonJS instead of ESM
-  },
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false }; // Fix issues with some dependencies
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
